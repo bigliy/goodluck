7ddba682b9122f1c9945c39002164f9e57098faa
bigliy <31265334+bigliy@users.noreply.github.com>
2017-08-23 13:37:27 +0800

    Update GoodLuck.java

--- GoodLuck.java
+++ GoodLuck.java
@@ -4,6 +4,7 @@ public class GoodLuck {
 	public static void main(String[] args) {
 		System.out.println("Gook Luck");
 		System.out.println("Think you");
+		System.out.println("Good bye");
 	}
 	
 }
